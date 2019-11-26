903a6a1f17ab1208d80d85956bae7e5ee933ca16
diegotbl <diegoblima2010@gmail.com>
2019-11-25 22:43:23 -0300

    Basic Nodejs Application

---
+++ Application/server/database.js
@@ -0,0 +1,78 @@
+/**
+ * Postgres DB Module
+ */
+const postgres = require('pg')
+const connectionString = process.env.DATABASE_URL
+
+// Initialize postgres client
+const client = new postgres.Client({ connectionString })
+
+// Connect to the DB
+client.connect().then(() => {
+  console.log(`Connected To ${client.database} at ${client.host}:${client.port}`)
+}).catch(console.log(`Error connecting ${client.database} at ${client.host}:${client.port}`))
+
+module.exports = {
+  /** Query the current time */
+  queryTime: async () => {
+    const result = await client.query('SELECT NOW() as now')
+    return result.rows[0]
+  },
+
+  /** Query the locations as geojson, for a given type */
+  getLocations: async (type) => {
+    const locationQuery = `
+      SELECT ST_AsGeoJSON(geog), name, type, gid
+      FROM locations
+      WHERE UPPER(type) = UPPER($1);`
+    const result = await client.query(locationQuery, [ type ])
+    return result.rows
+  },
+
+  /** Query the kingdom boundaries */
+  getKingdomBoundaries: async () => {
+    const boundaryQuery = `
+      SELECT ST_AsGeoJSON(geog), name, gid
+      FROM kingdoms;`
+    const result = await client.query(boundaryQuery)
+    return result.rows
+  },
+
+  /** Calculate the area of a given region, by id */
+  getRegionSize: async (id) => {
+    const sizeQuery = `
+        SELECT ST_AREA(geog) as size
+        FROM kingdoms
+        WHERE gid = $1
+        LIMIT(1);`
+    const result = await client.query(sizeQuery, [ id ])
+    return result.rows[0]
+  },
+
+  /** Count the number of castles in a region, by id */
+  countCastles: async (regionId) => {
+    const countQuery = `
+      SELECT count(*)
+      FROM kingdoms, locations
+      WHERE ST_intersects(kingdoms.geog, locations.geog)
+      AND kingdoms.gid = $1
+      AND locations.type = 'Castle';`
+    const result = await client.query(countQuery, [ regionId ])
+    return result.rows[0]
+  },
+
+  /** Get the summary for a location or region, by id */
+  getSummary: async (table, id) => {
+    if (table !== 'kingdoms' && table !== 'locations') {
+      throw new Error(`Invalid Table - ${table}`)
+    }
+
+    const summaryQuery = `
+        SELECT summary, url
+        FROM ${table}
+        WHERE gid = $1
+        LIMIT(1);`
+    const result = await client.query(summaryQuery, [ id ])
+    return result.rows[0]
+  }
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # A DB of ICE and FIRE
 
 Projeto de banco de dados baseado na obra do autor George R. R. Martin
+
+Aplicação baseada em https://blog.patricktriest.com/game-of-thrones-map-node-postgres-redis/
