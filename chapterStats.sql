5ab6580fd5e9fba4bb80ff363ef5587970553397
peerre <36823660+peerre@users.noreply.github.com>
2019-11-27 04:35:42 -0300

    oops

--- chapterStats.sql
+++ chapterStats.sql
@@ -1,14 +1,7 @@
 SELECT
-    ID_Book, POV, MAX(POV_Sequence) AS Num_Chapters
+    POV, 
+    (SELECT COUNT(POV)
+    FROM Chapter c
+    WHERE c.POV = POV) AS Total_Chapters
 FROM Chapter
-GROUP BY ID_Book, POV;
-
-SELECT
-    POV, SUM(Num_Chapters) AS Total_Chapters
-FROM (SELECT
-        ID_Book, POV, MAX(POV_Sequence) AS Num_Chapters
-    FROM Chapter
-    GROUP BY ID_Book, POV
-    )
-GROUP BY POV
 ORDER BY Total_Chapters;
